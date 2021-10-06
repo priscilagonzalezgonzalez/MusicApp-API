08a04d1dc67968f5583aace3be41ad303e8f8de8
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-10-06 15:36:05 -0500

    Ahora el modulo se maneja por clases

--- conexion.py
+++ conexion.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import hashlib
 
 #database connection
 db = mysql.connector.connect(
@@ -8,102 +9,99 @@ db = mysql.connector.connect(
 #cursor
 cursor = db.cursor()
 
-def existe_usuario(correo):
-    query = "SELECT COUNT(*) FROM usuario WHERE correo = %s"
-    cursor.execute(query, (correo,))
+class Usuarios:
+    @classmethod
+    def existe_usuario(self, correo):
+        query = "SELECT COUNT(*) FROM usuario WHERE correo = %s"
+        cursor.execute(query, (correo,))
 
-    if cursor.fetchone()[0] == 1:
-        return True
-    else:
-        return False
+        if cursor.fetchone()[0] == 1:
+            return True
+        else:
+            return False
 
-import hashlib
-def crear_usuario(nombre, apellido, correo, contra):
-    if existe_usuario(correo):
-        return False
-    else:
+    @classmethod
+    def crear_usuario(self, nombre, apellido, correo, contra):
+        if self.existe_usuario(correo):
+            return False
+        else:
+            h = hashlib.new('sha256', bytes(contra, 'utf-8'))
+            h = h.hexdigest()
+            insert_query = "INSERT INTO usuario(correo, contrasenia, nombre, apellido) VALUES (%s, %s, %s, %s)"
+            cursor.execute(insert_query, (correo, h, nombre, apellido))
+            db.commit()
+            return True
+
+    @classmethod
+    def iniciar_sesion(self, correo, contra):
         h = hashlib.new('sha256', bytes(contra, 'utf-8'))
         h = h.hexdigest()
-        insert_query = "INSERT INTO usuario(correo, contrasenia, nombre, apellido) VALUES (%s, %s, %s, %s)"
-        cursor.execute(insert_query, (correo, h, nombre, apellido))
+        query = "SELECT id FROM usuario WHERE correo = %s AND contrasenia = %s"
+        cursor.execute(query, (correo, h))
+        id = cursor.fetchone()
+        if id: #Si existe coincidencia
+            return id[0], True
+        else: 
+            return None, False
+
+class Artistas:
+    @classmethod
+    def get_artista_id(self, nombre):
+        query = "SELECT id FROM artista WHERE nombre = %s"
+        cursor.execute(query, (nombre,))
+        id = cursor.fetchone()
+        if id: #Si existe coincidencia
+            return id[0]
+        else: 
+            return None
+    
+    @classmethod
+    def existe_artista(self, nombre):
+        query = "SELECT COUNT(*) FROM artista WHERE nombre = %s"
+        cursor.execute(query, (nombre,))
+
+        if cursor.fetchone()[0] == 1:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def insertar_artista(self, nombre):
+        insertar = "INSERT INTO artista(nombre) VALUES (%s)"
+        cursor.execute(insertar, (nombre,))
         db.commit()
 
-        return True
-
-def iniciar_sesion(correo, contra):
-    h = hashlib.new('sha256', bytes(contra, 'utf-8'))
-    h = h.hexdigest()
-    query = "SELECT id FROM usuario WHERE correo = %s AND contrasenia = %s"
-    cursor.execute(query, (correo, h))
-    id = cursor.fetchone()
-    if id: #Si existe coincidencia
-        return id[0], True
-    else: 
-        return None, False
-
-def get_artista_id(nombre):
-    query = "SELECT id FROM artista WHERE nombre = %s"
-    cursor.execute(query, (nombre,))
-    id = cursor.fetchone()
-    if id: #Si existe coincidencia
-        return id[0]
-    else: 
-        return None
-
-def existe_artista(nombre):
-    query = "SELECT COUNT(*) FROM artista WHERE nombre = %s"
-    cursor.execute(query, (nombre,))
-
-    if cursor.fetchone()[0] == 1:
-        return True
-    else:
-        return False
-
-def insertar_artista(nombre):
-    insertar = "INSERT INTO artista(nombre) VALUES (%s)"
-    cursor.execute(insertar, (nombre,))
-    db.commit()
-
-    if cursor.rowcount:
-        return True
-    else:
-        return False
-
-
-def existe_album(titulo, artista):
-    query = "SELECT COUNT(*) FROM album WHERE titulo = %s AND artistaId = (SELECT id FROM artista WHERE nombre = %s)"
-    cursor.execute(query, (titulo, artista))
-
-    if cursor.fetchone()[0] == 1:
-        return True
-    else:
-        return False
-
-#Si no existe el artista, no existe el album
-def insertar_album(album):
-    titulo = album['titulo']
-    anio = album['anio']
-    imagen = album['imagen']
-    usuarioId = album['usuarioId']
-    nombre_artista = album['artista']
-
-    #Validar si existe album
-    if existe_album(titulo, nombre_artista):
-        return False
-    else:
-        if existe_artista(nombre_artista):
-            artista_id = get_artista_id(nombre_artista)
-            insert_query = "INSERT INTO album (titulo, anio, imagen, usuarioId, artistaId) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(insert_query, (titulo, anio, imagen, usuarioId, artista_id))
-            db.commit()
-
-            if cursor.rowcount:
-                return True
-            else:
-                return False
+        if cursor.rowcount:
+            return True
         else:
-            if insertar_artista(nombre_artista):
-                artista_id = get_artista_id(nombre_artista)
+            return False
+
+class Albums:
+    @classmethod
+    def existe_album(self, titulo, artista):
+        query = "SELECT COUNT(*) FROM album WHERE titulo = %s AND artistaId = (SELECT id FROM artista WHERE nombre = %s)"
+        cursor.execute(query, (titulo, artista))
+
+        if cursor.fetchone()[0] == 1:
+            return True
+        else:
+            return False
+
+    #Si no existe el artista, no existe el album
+    @classmethod
+    def insertar_album(self, album):
+        titulo = album['titulo']
+        anio = album['anio']
+        imagen = album['imagen']
+        usuarioId = album['usuarioId']
+        nombre_artista = album['artista']
+
+        #Validar si existe album
+        if self.existe_album(titulo, nombre_artista):
+            return False
+        else:
+            if Artistas.existe_artista(nombre_artista):
+                artista_id = Artistas.get_artista_id(nombre_artista)
                 insert_query = "INSERT INTO album (titulo, anio, imagen, usuarioId, artistaId) VALUES (%s, %s, %s, %s, %s)"
                 cursor.execute(insert_query, (titulo, anio, imagen, usuarioId, artista_id))
                 db.commit()
@@ -112,24 +110,36 @@ def insertar_album(album):
                     return True
                 else:
                     return False
-            else: #No se pudo insertar el artista, no se puede crear el album
-                return False
+            else:
+                if Artistas.insertar_artista(nombre_artista):
+                    artista_id = Artistas.get_artista_id(nombre_artista)
+                    insert_query = "INSERT INTO album (titulo, anio, imagen, usuarioId, artistaId) VALUES (%s, %s, %s, %s, %s)"
+                    cursor.execute(insert_query, (titulo, anio, imagen, usuarioId, artista_id))
+                    db.commit()
 
-def get_albums():
-    query = "SELECT id, titulo, anio, imagen, usuarioId, artistaId FROM album"
-    cursor.execute(query)
-    albums = []
-    for row in cursor.fetchall():
-        album = {
-            'id': row[0],
-            'titulo': row[1],
-            'anio': row[2],
-            'imagen': row[3],
-            'usuarioId': row[4],
-            'artistaId': row[5]
-        }
-        albums.append(album)
-    return albums
+                    if cursor.rowcount:
+                        return True
+                    else:
+                        return False
+                else: #No se pudo insertar el artista, no se puede crear el album
+                    return False
+
+    @classmethod
+    def get_albums(self):
+        query = "SELECT id, titulo, anio, imagen, usuarioId, artistaId FROM album"
+        cursor.execute(query)
+        albums = []
+        for row in cursor.fetchall():
+            album = {
+                'id': row[0],
+                'titulo': row[1],
+                'anio': row[2],
+                'imagen': row[3],
+                'usuarioId': row[4],
+                'artistaId': row[5]
+            }
+            albums.append(album)
+        return albums
 
 
 
