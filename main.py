c256cbb270ce94c35147f7e86b99a67bcde3cda6
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-18 14:06:45 -0600

    Ruta para obtener todos los artistas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, json, request, jsonify
 
 from conexion import Usuarios, Artistas, Albums
 
@@ -12,7 +12,7 @@ def usuario(id = None):
     if request.method == "POST" and request.is_json:
         try: 
             data = request.get_json()
-
+            
             if Usuarios.crear_usuario(data['nombre'], data['apellido'], data['correo'], data['contraseña']):
                 return jsonify({"code": "ok"})
             else:
@@ -53,8 +53,8 @@ def sesion():
         except:
             return jsonify({"code": "error"})
 
-@app.route("/api/v1/artistas", methods=["POST"])
-def artistas():
+@app.route("/api/v1/artistas", methods=["GET", "POST"])
+def artistas(id=None):
     if request.method == "POST" and request.is_json:
         try:
             data = request.get_json()
@@ -65,7 +65,11 @@ def artistas():
                 return jsonify({"code":"no"})
         except:
             return jsonify({"code":"error"})
-    else:
-        return jsonify({"code":"None"})
+    elif request.method == "GET" and id is None:
+        try:
+            return jsonify(Artistas.get_artistas())
+        except:
+            return jsonify({"code":"error"})
+    return jsonify({"code":"None"})
 
 app.run(debug=True)
